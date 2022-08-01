f387d99b4f359f91fdb5a27f5288804c993e9213
Jztemperor <mozsardani.jzt@gmail.com>
2022-08-01 20:20:52 +0200

    Components refactored

--- src/components/Deals.tsx
+++ src/components/Deals.tsx
@@ -1,11 +1,13 @@
 import React, { FC } from 'react';
 import styles from './Deals.module.css';
 import DealsForm from './DealsForm';
+import DealsList from './DealsList';
 
 const Deals: FC = () => {
   return (
     <div className={styles.container}>
       <DealsForm></DealsForm>
+      <DealsList></DealsList>
     </div>
   );
 };
---
+++ src/components/DealsList.tsx
@@ -0,0 +1,7 @@
+import React, { FCq } from 'react';
+
+const DealsList: FC = () => {
+  return <div>DealsList</div>;
+};
+
+export default DealsList;
